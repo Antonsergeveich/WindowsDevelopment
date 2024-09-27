98166f9d5c14846830700c448c4af99c754fd276
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-27 22:09:01 +0300

    Add project files.

---
+++ WinAPI/main.cpp
@@ -0,0 +1,75 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdSHow)
+{
+	//hInstance - экземпляр запущенного *.exe-файла нашей программы
+	//hPrevInst - НЕ используется
+	//LPSTR - Long Pointer To String
+	//lpCmdLine - CommandLine (командная строка с параметрами запуска приложения)
+	//nCmdShow - режим отображения окна (развёрнуто на весь экран, свёрнуто на панель задач)
+	//Префиксы: n...., lp... это Венгерская нотация
+	//          n - Number
+	//          lp - Long Pointer
+	//          h - HINSTANCE
+
+	//MessageBox
+	//(
+	//	NULL,
+	//	"Hello Windows!\nThis is MessegeBox", 
+	//	"Window title", 
+	//	MB_CANCELTRYCONTINUE | MB_HELP | MB_DEFBUTTON3 |
+	//	MB_ICONINFORMATION |
+	//	//MB_SYSTEMMODAL
+	//	//MB_DEFAULT_DESKTOP_ONLY |
+	//	MB_TOPMOST |
+	//	MB_RIGHT
+	//); 
+	//https://learn.microsoft.com/ru-ru/dotnet/api/system.windows.forms.messagebox?view=windowsdesktop-6.0
+	//MessegeBox: MB_ICONWARNING , MB_ICONERROR, MB_ICONINFORMATION
+	//wchar_t progect->propertis->Advanced->Character Set->Use Multi-Byte Character Set
+	//Модальным называется окно которое блокирует родительское окно
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+
+	return 0;
+}
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	//hwnd - Handler to Window. Обработчик или дискриптор окна - это число при помощи которого можно обратиться к окну.
+	//uMsg - Messege. Сообщение которое отправляется окну.
+	//wParam, lParam - это параметры сообщения, у каждого сообщения свой набор параметров.
+	//В процедуру окна максимально можно передать четыри параметра сообщения за один вызов.
+
+	switch (uMsg)
+	{
+	case WM_INITDIALOG: //Это сообщение отправляется один раз при инициализации окна
+		break;
+	case WM_COMMAND:    //Обрабатывает нажатие кнопок и другие действия пользователя
+		//ResourseID - самое обычное число типа INT
+		switch (LOWORD(wParam))
+		{
+		case IDC_BUTTON_COPY:
+		{
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {}; 
+			//sz - String Zero (NULL Terminated Line - C -string)
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:      //Отправляется при нажатии кнопки закрыть 'X'
+		EndDialog(hwnd, 0);
+		//C:\Users\Pro>taskkill /f /im WinAPI.exe (закрыть окно через консоль)
+		break;
+	}
+	return FALSE;
+}
